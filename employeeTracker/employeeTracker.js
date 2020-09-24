893db4d847941f9c12227bdf6ffdbfa9587b9f64
marcuspramirez <marcus.p.ramirez2020@gmail.com>
2020-09-24 02:38:47 -0500

    added new role prompts

--- employeeTracker/employeeTracker.js
+++ employeeTracker/employeeTracker.js
@@ -1,6 +1,7 @@
 // Dependencies
 var express = require("express");
 var mysql = require("mysql");
+var inquirer = require("inquirer");
 
 // Create express app instance.
 var app = express();
@@ -19,18 +20,138 @@ var connection = mysql.createConnection({
 });
 
 // Initiate MySQL Connection.
-connection.connect(function(err) {
+connection.connect(function (err) {
   if (err) {
     console.error("error connecting: " + err.stack);
     return;
   }
   console.log("connected as id " + connection.threadId);
+  start();
 });
+
+function start() {
+  inquirer
+    .prompt({
+      name: "action",
+      type: "list",
+      message: "What would you like to do?",
+      choices: [
+        "Add a Role, Department or Employee",
+        "View a Role, Department or Employee",
+        "Update Employee Roles",
+        "Exit"
+      ]
+    })
+    .then(function (answer) {
+      switch (answer.action) {
+        case "Add a Role, Department or Employee":
+          add();
+          break;
+
+        case "View a Role, Department or Employee":
+          view();
+          break;
+
+        case "Update Employee Roles":
+          update();
+          break;
+
+        case "Exit":
+          connection.end();
+          break;
+      }
+    });
+}
+
+function add() {
+  inquirer
+    .prompt({
+      name: "action",
+      type: "list",
+      message: "What would you like to add?",
+      choices: [
+        "Role",
+        "Department",
+        "Employee",
+        "Exit"
+      ]
+    })
+    .then(function (answer) {
+      switch (answer.action) {
+        case "Role":
+          addRole();
+          break;
+
+        case "Department":
+          addDepartment();
+          break;
+
+        case "Employee":
+          addEmployee();
+          break;
+
+        case "Exit":
+          connection.end();
+          break;
+      }
+    });
+}
+
+function addRole() {
+  inquirer
+    .prompt([
+      {
+        name: "roleTitle",
+        type: "input",
+        message: "What is the role title?",
+        
+      },
+      {
+        name: "roleSalary",
+        type: "input",
+        message: "What is the role salary?",
+        
+      },
+      {
+
+        name: "roleDepartmentID",
+        type: "input",
+        message: "What is the role's department ID?",
+      
+      }
+    ])
+
+}
+
+// function addDepartment() {
+//   inquirer
+//     .prompt([
+//       {
+//         name: "roleTitle",
+//         type: "input",
+//         message: "What is the role title?",
+        
+//       },
+//       {
+//         name: "roleSalary",
+//         type: "input",
+//         message: "What is the role salary?",
+        
+//       },
+//       {
+
+//         name: "roleDepartmentID",
+//         type: "input",
+//         message: "What is the role's department ID?",
+      
+//       }
+//     ])
+
+// }
 
 
 // Start our server so that it can begin listening to client requests.
-app.listen(PORT, function() {
-    // Log (server-side) when our server has started
-    console.log("Server listening on: http://localhost:" + PORT);
-  });
-  
+app.listen(PORT, function () {
+  // Log (server-side) when our server has started
+  console.log("Server listening on: http://localhost:" + PORT);
+});
