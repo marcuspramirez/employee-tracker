2b05a6013d307b8788d5897b12dfcc5b6d5933cc
marcuspramirez <marcus.p.ramirez2020@gmail.com>
2020-09-24 00:06:35 -0500

    added connection to sql

---
+++ employeeTracker/employeeTracker.js
@@ -0,0 +1,36 @@
+// Dependencies
+var express = require("express");
+var mysql = require("mysql");
+
+// Create express app instance.
+var app = express();
+
+// Set the port of our application
+// process.env.PORT lets the port be set by Heroku
+var PORT = process.env.PORT || 8080;
+
+// MySQL DB Connection Information (remember to change this with our specific credentials)
+var connection = mysql.createConnection({
+  host: "localhost",
+  port: 3306,
+  user: "root",
+  password: "",
+  database: "employee_tracker_db"
+});
+
+// Initiate MySQL Connection.
+connection.connect(function(err) {
+  if (err) {
+    console.error("error connecting: " + err.stack);
+    return;
+  }
+  console.log("connected as id " + connection.threadId);
+});
+
+
+// Start our server so that it can begin listening to client requests.
+app.listen(PORT, function() {
+    // Log (server-side) when our server has started
+    console.log("Server listening on: http://localhost:" + PORT);
+  });
+  
